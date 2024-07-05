deeab87ab79221ffed46b8059270789ddcc52df0
renalsugiana <a22100107@mhs.stmik-sumedang.ac.id>
2024-07-05 21:57:36 +0700

    update

--- A2.2100107/Project/personalblog/resources/views/about.blade.php
+++ A2.2100107/Project/personalblog/resources/views/about.blade.php
@@ -2,7 +2,6 @@
 
 @section('container')
     <h1>Profil Developer</h1>
-    <img src="img/{{ $profil }}" alt="{{ $nama }}" width="200" class="img-thumnail rounded-circle">
+    <img src="img/{{ $profil }}" alt="{{ $nama }}" width="200" class="img-thumnail rounded-circle my-4">
     <h3>Nama : {{ $nama }}</h3>
-    <h3>Email : {{ $email }}</h3>
 @endsection
